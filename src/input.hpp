409dc9a271a02965d1c67c2b11cadbc1ed5841ae
truemorgandev <truemorgan112@gmail.com>
2025-09-25 14:52:32 +0300

    Feature: added input feature

---
+++ src/input.hpp
@@ -0,0 +1,33 @@
+#ifndef INPUT_HPP
+#define INPUT_HPP
+#include "Window.hpp"
+
+class Input 
+{
+public:
+    static bool init();
+    static void terminate();
+
+    static bool getKey(int keycode);
+    static bool getKeyDown();
+
+    static bool getMouseButton();
+    static bool getMouseButtonDown();
+
+    static float mouseX, mouseY;
+    static bool cursorLocked;
+    static bool cursorStarted;
+
+    static bool* keys;
+    static uint* frames;
+    static uint current;
+
+    static float deltaY, deltaX;
+
+    static void pollEvents();
+
+};
+
+
+
+#endif 
