c79f9c0969f3cfef73d221e576a6b34549668681
truemorgandev <truemorgan112@gmail.com>
2025-09-26 09:35:16 +0300

    Feature: added shaders managment

---
+++ src/shader.hpp
@@ -0,0 +1,18 @@
+#ifndef SHADER_HPP
+#define SHADER_HPP
+
+#include <string>
+#include <GL/glew.h>
+
+class Shader {
+public:
+    GLuint ID;
+
+    Shader(const char* vertexSource, const char* fragmentSource);
+    void use();
+
+private:
+    GLuint compileShader(GLenum type, const char* source);
+};
+
+#endif
