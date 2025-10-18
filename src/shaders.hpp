ff412b147f8e436078b8033cc89bc98666109be9
truemorgandev <truemorgan112@gmail.com>
2025-10-18 09:17:05 +0300

    Added license

--- src/shaders.hpp
+++ src/shaders.hpp
@@ -75,4 +75,53 @@ void main() {
 }
 )";
 
+
+static const char* unlitVertex = R"(
+#version 330 core
+
+layout (location = 0) in vec3 position;
+layout (location = 1) in vec3 normal;
+layout (location = 2) in vec2 uv;
+
+out vec2 texCoord;
+out vec3 fragNormal;
+
+uniform mat4 transform;
+uniform mat4 projection;
+uniform mat4 view;
+
+void main() {
+    gl_Position = projection * view * transform * vec4(position, 1.0);
+    texCoord = uv;
+    fragNormal = normal;
+}
+)";
+
+/**
+ * @brief Fragment shader for lit objects.
+ *
+ * Inputs:
+ * - `texCoord` : Interpolated texture coordinates from vertex shader.
+ * - `fragNormal`: Interpolated normal from vertex shader.
+ *
+ * Uniforms:
+ * - `_texture` : Texture sampler.
+ *
+ * Outputs:
+ * - `color` : Final fragment color.
+ */
+static const char* unlitFragment = R"(
+#version 330 core
+
+out vec4 color;
+in vec2 texCoord;
+in vec3 fragNormal;
+
+uniform sampler2D _texture;
+
+void main() {
+    color = texture(_texture, texCoord);
+}
+)";
+
 #endif // SHADERS_HPP
